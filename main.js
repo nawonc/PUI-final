2a9ea98079d6caf4c4e59b69a7c2fda78d6b0ef9
Nawon Choi <nawonc@andrew.cmu.edu>
2018-11-30 17:18:55 -0500

    ready to go:

--- main.js
+++ main.js
@@ -19,20 +19,58 @@ $(document).ready(function() {
   $('#radio-group').click(function() {
     var beeType = $('input[name=selector]:checked').val(); 
     if (beeType == 'queen') {
-      $('#queen-content').show();
+      $('#queen-content').slideToggle('slow', 'swing');
       $("#worker-content").hide();
       $("#drone-content").hide();
       
     } else if (beeType == 'worker') {
-      $('#worker-content').show();
+      $('#worker-content').slideToggle('slow', 'swing');
       $("#queen-content").hide();
       $("#drone-content").hide();
     } else {
-      $('#drone-content').show();
+      $('#drone-content').slideToggle('slow', 'swing');
       $("#queen-content").hide();
       $("#worker-content").hide();
     }
-
   });
 
+
+  // navigation hover
+  $('.hexagon.home').mouseover(function() {
+    $('#hex-home').css("visibility","visible");
+  });
+  $('.hexagon.home').mouseout(function() {
+    $('#hex-home').css("visibility","hidden");
+  });
+  $('.hexagon.about').mouseover(function() {
+    $('#hex-about').css("visibility","visible");
+  });
+  $('.hexagon.about').mouseout(function() {
+    $('#hex-about').css("visibility","hidden");
+  });
+  $('.hexagon.hive').mouseover(function() {
+    $('#hex-hive').css("visibility","visible");
+  });
+  $('.hexagon.hive').mouseout(function() {
+    $('#hex-hive').css("visibility","hidden");
+  });
+  $('.hexagon.buzz').mouseover(function() {
+    $('#hex-buzz').css("visibility","visible");
+  });
+  $('.hexagon.buzz').mouseout(function() {
+    $('#hex-buzz').css("visibility","hidden");
+  });
+  $('.hexagon.effects').mouseover(function() {
+    $('#hex-effects').css("visibility","visible");
+  });
+  $('.hexagon.effects').mouseout(function() {
+    $('#hex-effects').css("visibility","hidden");
+  });
+  $('.hexagon.protect').mouseover(function() {
+    $('#hex-protect').css("visibility","visible");
+  });
+  $('.hexagon.protect').mouseout(function() {
+    $('#hex-protect').css("visibility","hidden");
+  });
+  
 });
