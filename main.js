61d4d5d4aca4154e4291136f3b3ed8a962c5a9df
Nawon Choi <nawonc@andrew.cmu.edu>
2018-11-30 00:38:23 -0500

    adding animations

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-function navSelected() {
- $('.hex-container.active').attr()
-}
-
 $(document).ready(function() {
-  navSelected();
+  // ScrollReveal().reveal('#home-title'), { delay: 800 };
+  // ScrollReveal().reveal('.page-title'), { delay: 500 };
+  // ScrollReveal().reveal('.about-honeybee', { delay: 500 });
+  // ScrollReveal().reveal('.about-text', { delay: 500 });
+  // ScrollReveal().reveal('.hive-text', { delay: 500 });
+  // ScrollReveal().reveal('.bee-btns', { delay: 500 });
+
+  $("#worker-content").hide();
+  $("#drone-content").hide();
+
+  $(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+
+  $('#radio-group').click(function() {
+    var beeType = $('input[name=selector]:checked').val(); 
+    if (beeType == 'queen') {
+      $('#queen-content').show();
+      $("#worker-content").hide();
+      $("#drone-content").hide();
+      
+    } else if (beeType == 'worker') {
+      $('#worker-content').show();
+      $("#queen-content").hide();
+      $("#drone-content").hide();
+    } else {
+      $('#drone-content').show();
+      $("#queen-content").hide();
+      $("#worker-content").hide();
+    }
+
+  });
+
 });
